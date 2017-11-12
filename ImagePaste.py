fb34685ca60449006d3ed8dbf86f2f31e129fe9a
robin.chenyu <robin.chenyu@gmail.com>
2017-11-12 23:24:45 +0800

    add Linux support

--- ImagePaste.py
+++ ImagePaste.py
@@ -3,19 +3,39 @@ import sublime_plugin
 import os
 import sys
 import re
-
-package_file = os.path.normpath(os.path.abspath(__file__))
-package_path = os.path.dirname(package_file)
-lib_path =  os.path.join(package_path, "lib")
-if lib_path not in sys.path:
-    sys.path.append(lib_path)
-    print(sys.path)
-from PIL import ImageGrab
-from PIL import ImageFile
-from PIL import Image
+import subprocess
+import shutil
 
 
-class ImagePasteCommand(sublime_plugin.TextCommand):
+# package_file = os.path.normpath(os.path.abspath(__file__))
+# package_path = os.path.dirname(package_file)
+# lib_path =  os.path.join(package_path, "lib")
+# if lib_path not in sys.path:
+#     sys.path.append(lib_path)
+#     print(sys.path)
+# from PIL import ImageGrab
+# from PIL import ImageFile
+# from PIL import Image
+
+class ImageCommand(object):
+
+	def run_command(self, cmd):
+		cwd = os.path.dirname(self.view.file_name())
+		print("cmd %r" % cmd)
+		proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=os.environ)
+		
+		try:
+		    outs, errs = proc.communicate(timeout=15)
+		    print("outs %r %r" % (outs, proc))
+		except TimeoutExpired:
+		    proc.kill()
+		    outs, errs = proc.communicate()
+		print("outs %r, errs %r" % (b'\n'.join(outs.split(b'\r\n')), errs))
+		if errs is None or len(errs) == 0:
+			return outs.decode()
+		
+
+class ImagePasteCommand(ImageCommand, sublime_plugin.TextCommand):
 	def run(self, edit):
 		view = self.view
 		rel_fn = self.paste()
@@ -33,12 +53,21 @@ class ImagePasteCommand(sublime_plugin.TextCommand):
 			
 
 	def paste(self):
-		ImageFile.LOAD_TRUNCATED_IMAGES = True
-		im = ImageGrab.grabclipboard()
-		if im:
-			abs_fn, rel_fn = self.get_filename()
-			im.save(abs_fn,'PNG')	
+		# ImageFile.LOAD_TRUNCATED_IMAGES = True
+		dirname = os.path.dirname(self.view.file_name())
+		command = ['/usr/bin/python3', os.path.join(dirname, 'bin/imageutil.py'), 'save']
+		abs_fn, rel_fn = self.get_filename()
+		command.append(abs_fn)
+
+		out = self.run_command(" ".join(command))
+		if out[:4] == "save":
 			return rel_fn
+
+		# im = ImageGrab.grabclipboard()
+		# if im:
+		# 	abs_fn, rel_fn = self.get_filename()
+		# 	im.save(abs_fn,'PNG')	
+		# 	return rel_fn
 		else:
 			print('clipboard buffer is not image!')
 			return None
@@ -67,13 +96,81 @@ class ImagePasteCommand(sublime_plugin.TextCommand):
 		print("save file: " + abs_filename + "\nrel " + rel_filename)
 		return abs_filename, rel_filename
 
+class ImageGrabCommand(ImageCommand, sublime_plugin.TextCommand):
+	def run(self, edit):
+		view = self.view
+		rel_fn = self.paste()
+		if not rel_fn:
+			view.run_command("paste")
+			return
+		for pos in view.sel():
+			# print("scope name: %r" % (view.scope_name(pos.begin())))
+			if 'text.html.markdown' in view.scope_name(pos.begin()):
+				view.insert(edit, pos.begin(), "![](%s)" % rel_fn)
+			else:
+				view.insert(edit, pos.begin(), "%s" % rel_fn)
+			# only the first cursor add the path
+			break
+			
 
-class ImagePreviewCommand(sublime_plugin.TextCommand):
+	def paste(self):
+		# ImageFile.LOAD_TRUNCATED_IMAGES = True
+		dirname = os.path.dirname(self.view.file_name())
+		command = ['/usr/bin/python3', os.path.join(dirname, 'bin/imageutil.py'), 'grab']
+		abs_fn, rel_fn = self.get_filename()
+		tempfile1 = "/tmp/imagepaste1.png"
+		command.append(tempfile1)
+
+		out = self.run_command(" ".join(command))
+		if out[:4] == "grab":
+			ret = sublime.ok_cancel_dialog("save to file?")
+			print("ret %r" % ret)
+			if ret:
+				shutil.move(tempfile1, abs_fn)
+				return rel_fn
+			else:
+				return None
+		# im = ImageGrab.grabclipboard()
+		# if im:
+		# 	abs_fn, rel_fn = self.get_filename()
+		# 	im.save(abs_fn,'PNG')	
+		# 	return rel_fn
+		else:
+			print('clipboard buffer is not image!')
+			return None
+
+	def get_filename(self):
+		view = self.view
+		filename = view.file_name()
+
+		# create dir in current path with the name of current filename
+		dirname, _ = os.path.splitext(filename)
+
+		# create new image file under currentdir/filename_without_ext/filename_without_ext%d.png
+		fn_without_ext = os.path.basename(dirname)
+		if not os.path.lexists(dirname):
+			os.mkdir(dirname)
+		i = 0
+		while True:
+			# relative file path
+			rel_filename = os.path.join("%s/%s%d.png" % (fn_without_ext, fn_without_ext, i))
+			# absolute file path
+			abs_filename = os.path.join(dirname, "%s%d.png" % ( fn_without_ext, i))
+			if not os.path.exists(abs_filename):
+				break
+			i += 1
+
+		print("save file: " + abs_filename + "\nrel " + rel_filename)
+		return abs_filename, rel_filename
+
+class ImagePreviewCommand(ImageCommand, sublime_plugin.TextCommand):
 	def __init__(self, *args):
 	#	self.view = view
 		super(ImagePreviewCommand, self).__init__(*args)	    
 		# self.phantom_set = sublime.PhantomSet(self.view)
 		self.displayed = False
+
+
 
 
 	def get_line(self):
@@ -97,11 +194,17 @@ class ImagePreviewCommand(sublime_plugin.TextCommand):
 				file2 = os.path.join(dirname, file1)
 				# print("%s = %s" % (name, file1))
 				region = tp
-				with Image.open(file2) as im:
-					# print("file: %s with size: %d %d" % (file1, im.width, im.height))
-					message = '''<body>
-					<img width="%d" height="%d" src="file://%s"></img>
-					</body>''' % (im.width, im.height, file2)
+
+				command = ['/usr/bin/python3', os.path.join(dirname, 'bin/imageutil.py'), 'size']
+				command.append(file2)
+
+				out = self.run_command(" ".join(command))
+				widthstr, heightstr = out.split(',')
+				# with Image.open(file2) as im:
+				# print("file: %s with size: %d %d" % (file1, im.width, im.height))
+				message = '''<body>
+				<img width="%s" height="%s" src="file://%s"></img>
+				</body>''' % (widthstr, heightstr, file2)
 				if len(name) == 0:
 					name = file1
 
---
+++ bin/imageutil.py
@@ -0,0 +1,81 @@
+"""
+for Linux use
+pip install PyUserInput, pyscreenshot 
+"""
+import sys
+import os
+from PIL import Image
+import pyscreenshot as ImageGrab
+from pymouse import PyMouse
+from pymouse import PyMouseEvent
+import time
+
+class event(PyMouseEvent):
+	def __init__(self):
+		super(event, self).__init__()
+		self.clicks = []
+		# FORMAT = '%(asctime)-15s ' + gethostname() + ' touchlogger %(levelname)s %(message)s'
+		# logging.basicConfig(filename='/var/log/mouse.log', level=logging.DEBUG, format=FORMAT)
+
+	def move(self, x, y):
+		pass
+
+	def click(self, x, y, button, press):
+		if press:
+			# print('{ "event": "click", "type": "press", "x": "' + str(x) + '", "y": "' + str(y) + '"}') 
+			self.clicks.append((x, y))
+		else:
+			pass
+			# print('{ "event": "click", "type": "release", "x": "' + str(x) + '", "y": "' + str(y) + '"}') 
+
+
+
+
+def getSize(filename):
+	with Image.open(filename) as im:
+		# print("file: %s with size: %d %d" % (file1, im.width, im.height))
+		return (im.width, im.height)
+
+def saveImagefile(filename):
+	im = ImageGrab.grab()
+	if im:
+		print("save")
+		im.save(filename)
+		return 0
+	else:
+		return 1
+
+def saveImagefile2(filename, pt1, pt2):
+	im = ImageGrab.grab(bbox=(pt1[0], pt1[1], pt2[0], pt2[1]))
+	if im:
+		print("grab %r %r %r" % (filename, pt1, pt2))
+		# ret = im.show()
+		# print("ret %r" % (ret))
+		im.save(filename)
+		return 0
+	else:
+		return 1
+
+# print(__name__, sys.argv)
+if __name__ == '__main__':
+	if len(sys.argv) == 3:
+		# python imageutil.py size filename
+		# print("begin ")
+		if sys.argv[1] == 'size':
+			print("%r,%r" % getSize(sys.argv[2]))
+		elif sys.argv[1] == 'save':
+			print(saveImagefile(sys.argv[2]))
+		elif sys.argv[1] == 'grab':
+			# print("grab123 %s" % os.environ.get('DISPLAY', ':0'))
+			e = event()
+			e.capture = False
+			e.daemon = True
+			e.start()
+
+			while(True):
+				time.sleep(1)
+				if len(e.clicks) == 2:
+					saveImagefile2(sys.argv[2], e.clicks[0], e.clicks[1])
+					break;
+
+		# print("end")
